829431f57251f214f8422873a4843102c915a4f5
JosiasLopes <jobercardinho2014@gmail.com>
2021-02-03 15:19:58 -0300

    Adicionando uma mensagem sobre o filme ou serie usando Jquery

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$("#informacoes").click(function(){
+	//alert($(".detalhes").css("visibility"));
+	if($(".detalhes").css("visibility")=='hidden' && $(".detalhes").css("display")=='none' ){
+		
+		//$(".detalhes").show();
+		$(".detalhes").animate({height: '100px', opacity: '1'}, "slow");
+		$(".detalhes").css({"display":"block","visibility":"visible"});
+		
+	}else{
+		$(".detalhes").animate({height: '0', opacity: '0'}, "slow");
+		$(".detalhes").css({"display":"none","visibility":"hidden"});
+		
+			
+	}
+	
+});
